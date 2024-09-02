0e64fd994e9866e07614f9516fb1472617d93414
MrHallKellett <chall@kellettschool.com>
2024-09-02 17:18:12 +0800

    reference actual assignments folder

--- new_autograding_system/config.py
+++ new_autograding_system/config.py
@@ -1,7 +1,10 @@
+from os import path
+
 ASS_PATH = "assignments"
-STU_WORK_PATH = "example_student_work"
+STU_WORK_PATH = path.join("..", "..", "Student Work 24_25")
 IGNORE = """input-output matching
 solutions
 tests.py""".splitlines()
 BACK_UP = True
 DB_FILENAME = "test_results.db"
+YEAR_GROUPS = ["10"]
